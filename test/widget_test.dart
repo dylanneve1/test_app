10533677acd99807fe1529d6e44ee87346e599f5
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-26 16:19:28 +0000

    Add me (2)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,5 @@
+// Copyright Dylan Neve <dylanneve1@gmail.com> 2018
+
 // This is a basic Flutter widget test.
 // To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
